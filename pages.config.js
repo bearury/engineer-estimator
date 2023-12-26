6cca3624d8721b437209138262593157231e3c16
webjsmaster <web.js.master@gmail.com>
2023-12-26 22:30:55 +0300

    feat:deploy

--- pages.config.js
+++ pages.config.js
@@ -2,5 +2,4 @@ import { resolve } from 'path'
 
 export const pages = [
 	{ name: 'index', path: resolve(__dirname, './index.html') },
-	{ name: 'menu', path: resolve(__dirname, './menu.html') },
 ]
--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,6 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: { ...pageInput },
-			// output: {
-			// 	dir: resolve(__dirname, './..'),
-			// },
 		},
 		sourcemap: true,
 	},
